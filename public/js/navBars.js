99047f08b884864d745fa096d51ca4f7c76f5661
enio.as14@outlook.com <enio.as14@outlook.com>
2026-01-09 17:28:57 -0300

    0.0.9

--- public/js/navBars.js
+++ public/js/navBars.js
@@ -9,29 +9,28 @@ document.querySelectorAll(".navbar-nav li a").forEach((link) => {
 });
 document.addEventListener("DOMContentLoaded", () => {
   const field = document.querySelector(".stars-field");
-
+  
   function createStar() {
     const star = document.createElement("span");
     star.classList.add("star");
 
-    const size =
-      Math.random() < 0.2
-        ? Math.random() * 3 + 3 // estrelas grandes
-        : Math.random() * 2 + 2; // estrelas normais
+    const size = Math.random() * 3 + 2;
+    const duration = Math.random() * 2.5 + 2.5;
 
-    const duration = Math.random() * 3 + 3;
+    const dx = (Math.random() - 0.5) * 30; // -15px a 15px
+    const dy = (Math.random() - 0.5) * 30;
 
     star.style.width = `${size}px`;
     star.style.height = `${size}px`;
     star.style.left = `${Math.random() * 100}%`;
     star.style.top = `${Math.random() * 100}%`;
     star.style.animationDuration = `${duration}s`;
+    star.style.setProperty("--dx", `${dx}px`);
+    star.style.setProperty("--dy", `${dy}px`);
 
     field.appendChild(star);
 
-    setTimeout(() => {
-      star.remove();
-    }, duration * 1000);
+    setTimeout(() => star.remove(), duration * 1000);
   }
 
   setInterval(createStar, 120);
